6b7119c86d22b9520e795e3299d1233cb86e2a83
BogdanDimov <bogdan.dimov@outlook.com>
2016-11-21 22:38:14 +0200

    update

---
+++ BinaryShort/BinaryShort.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace BinaryShort
+{
+    class BinaryShort
+    {
+        static string ShortToBin(short n)
+        {
+            string bin = string.Empty;
+            bool numberIsNegative = false;
+
+            if (n < 0)
+            {
+                if (n == Math.Pow(-2, 15))
+                {
+                    return "1000000000000000";
+                }
+
+                numberIsNegative = true;
+                n *= -1;
+            }
+            else if (n == 0)
+            {
+                return "0000000000000000";
+            }
+
+            while (n > 0)
+            {
+                bin = n % 2 + bin;
+                n /= 2;
+            }
+
+            for (int i = 0; i < bin.Length % 16; i++) //add missing zeros
+            {
+                bin = "0" + bin;
+            }
+
+            if (numberIsNegative)
+            {
+                char[] bin_c = new char[16]; // create char to store negated values
+                bin_c[0] = '1'; // MSB is always 1 in negative numbers.
+
+                for (int i = 1; i < bin.Length; i++)
+                {
+                    if (bin[i] == '1')
+                    {
+                        bin_c[i] = '0';
+                    }
+                    else
+                    {
+                        bin_c[i] = '1';
+                    }
+                }
+
+                bool carry = false;
+                if (bin_c[bin_c.Length - 1] == '1')
+                {
+                    carry = true;
+                    bin_c[bin_c.Length - 1] = '0';
+                }
+                else
+                {
+                    bin_c[bin_c.Length - 1] = '1';
+                }
+
+                for (int i = bin_c.Length - 2; i >= 0; i--)
+                {
+                    if (carry == true)
+                    {
+                        if (bin_c[i] == '0')
+                        {
+                            bin_c[i] = '1';
+                            carry = false;
+                        }
+                        else
+                        {
+                            bin_c[i] = '0';
+                        }
+                    }
+                }
+
+                return new string(bin_c);
+            }
+
+            return bin;
+        }
+
+        static void Main()
+        {
+            short n = short.Parse(Console.ReadLine());
+            Console.WriteLine(ShortToBin(n));
+        }
+    }
+}
---
+++ OneSystemToAnyOther/OneSystemToAnyOther.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace OneSystemToAnyOther
+{
+    class OneSystemToAnyOther
+    {
+        /// <summary>
+        /// Convert a positive number 'num' to its digit representation in base 'd'.
+        /// </summary>
+        static string ToDigits(long num, int d)
+        {
+            string digits = string.Empty;
+
+            while (num > 0)
+            {
+                string digitToAdd = Convert.ToString(num % d);
+                switch (digitToAdd)
+                {
+                    case "10": digitToAdd = "A"; break;
+                    case "11": digitToAdd = "B"; break;
+                    case "12": digitToAdd = "C"; break;
+                    case "13": digitToAdd = "D"; break;
+                    case "14": digitToAdd = "E"; break;
+                    case "15": digitToAdd = "F"; break;
+                }
+
+                digits = digitToAdd + digits;
+                num /= d;
+            }
+
+            return digits;
+        }
+
+        /// <summary>
+        /// Compute the number given by digits in base 's'.
+        /// </summary>
+        static long FromDigits(string digits, int s)
+        {
+            long n = 0;
+            foreach (var digit in digits)
+            {
+                if (digit >= 'A' && digit <= 'F')
+                {
+                    n = s * n + digit - 55;
+                }
+                else
+                {
+                    n = s * n + digit - '0';
+                }
+            }
+
+            return n;
+        }
+
+        /// <summary>
+        /// Convert the digits representation of a number from base 's' to base 'd'.
+        /// </summary>
+        static string ConvertBase(string digits, int s, int d)
+        {
+            return ToDigits(FromDigits(digits, s), d);
+        }
+
+        static void Main()
+        {
+            int s = int.Parse(Console.ReadLine());
+            string digits = Console.ReadLine();
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine(ConvertBase(digits, s, d));
+        }
+    }
+}
